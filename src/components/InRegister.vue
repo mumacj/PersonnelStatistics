117895fbf818a03e9debe60abf554daf670ecac5
mumacj <1063080476@qq.com>
2020-04-15 17:15:20 +0800

    完成出入登记-添加模块，出入登记动态获取数据；未完成查询，保存，删除，分页功能

--- src/components/InRegister.vue
+++ src/components/InRegister.vue
@@ -3,7 +3,9 @@
     <div style="margin-bottom:30px">
       <el-row>
         <el-col :span="12" align="center">
-          <span style="font-size:20px;"><b>起止时间：</b></span>
+          <span style="font-size:20px;">
+            <b>起止时间：</b>
+          </span>
           <el-date-picker
             v-model="dateForSearch"
             type="datetimerange"
@@ -14,21 +16,77 @@
           ></el-date-picker>
         </el-col>
         <el-col :span="6" align="center">
-          <span style="font-size:20px;"><b>姓名：</b></span>
+          <span style="font-size:20px;">
+            <b>姓名：</b>
+          </span>
           <el-input v-model="input" placeholder="请输入内容" style="width:50%"></el-input>
         </el-col>
         <el-col :span="6" align="center">
-          <el-button
-            size="medium"
-            type="primary"
-            icon="el-icon-search"
-          >查询</el-button>
-          <el-button
-            size="medium"
-            type="warning"
-            icon="el-icon-circle-plus-outline"
-          >添加</el-button>
+          <el-button size="medium" type="primary" icon="el-icon-search">查询</el-button>
+          <el-button size="medium" type="warning" icon="el-icon-circle-plus-outline" @click="add">添加</el-button>
         </el-col>
+
+        <!-- 添加记录 -->
+        <el-dialog title="添加记录" :visible.sync="dialogFormVisible" align="center">
+          <el-form
+            ref="form"
+            :model="sizeForm"
+            label-width="80px"
+            size="small"
+            style="width:50%"
+            label-position="left"
+            align="left"
+          >
+            <el-form-item label="证件号">
+              <el-input v-model="sizeForm.idCard" style="width:50%"></el-input>
+            </el-form-item>
+            <el-form-item label="姓名">
+              <el-input v-model="sizeForm.name" style="width:50%"></el-input>
+            </el-form-item>
+            <el-form-item label="时间">
+              <el-col :span="11">
+                <el-date-picker
+                  type="date"
+                  placeholder="选择日期"
+                  v-model="sizeForm.date"
+                  style="width: 100%;"
+                ></el-date-picker>
+              </el-col>
+              <el-col class="line" :span="2">-</el-col>
+              <el-col :span="11">
+                <el-time-picker placeholder="选择时间" v-model="sizeForm.time" style="width: 100%;"></el-time-picker>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="住户地址">
+              <el-input v-model="sizeForm.address" style="width:80%"></el-input>
+            </el-form-item>
+            <el-form-item label="当前体温">
+              <el-input v-model="sizeForm.tempNow" style="width:50%"></el-input>
+            </el-form-item>
+            <el-form-item label="是否离杭">
+              <el-radio-group v-model="sizeForm.wheLeave" size="medium">
+                <el-radio border label="是"></el-radio>
+                <el-radio border label="否"></el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="健康码">
+              <template>
+                <el-switch
+                  style="display: block;"
+                  v-model="sizeForm.healthCode"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949"
+                  active-text="健康"
+                  inactive-text="待观察"
+                ></el-switch>
+              </template>
+            </el-form-item>
+            <el-form-item size="large">
+              <el-button type="primary" @click="onSubmit">保存</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
+          </el-form>
+        </el-dialog>
       </el-row>
     </div>
     <el-table size="small" :data="tableData" height="540" stripe style="width: 100%">
@@ -42,7 +100,7 @@
             v-model="scope.row.dateTime"
             :readonly="scope.row.readonly"
           ></el-date-picker>
-          <span v-show="scope.row.readonly" @dblclick="change(scope.row)">{{scope.row.time}}</span>
+          <span v-show="scope.row.readonly" @dblclick="change(scope.row)">{{scope.row.dateTime}}</span>
         </template>
       </el-table-column>
       <el-table-column prop="name" label="姓名" width="130" align="center">
@@ -70,11 +128,11 @@
         </template>
       </el-table-column>
       <el-table-column prop="wheLeave" label="是否离杭" align="center"></el-table-column>
-      <el-table-column prop="healthMark" label="健康码" align="center">
+      <el-table-column prop="healthCode" label="健康码" align="center">
         <template slot-scope="scope">
           <el-switch
             style="display: block;"
-            v-model="scope.row.healthMark"
+            v-model="scope.row.healthCode"
             active-color="#13ce66"
             inactive-color="#ff4949"
             active-text="健康"
@@ -119,7 +177,9 @@
 export default {
   data() {
     return {
-      dateForSearch:"",
+      sizeForm: {},
+      dialogFormVisible: false,
+      dateForSearch: "",
       input: "",
       tableData: [
         {
@@ -131,7 +191,7 @@ export default {
           idCard: "339005123456789123",
           tempNow: "37.5",
           wheLeave: "否",
-          healthMark: true
+          healthCode: true
         }
       ]
     };
@@ -140,15 +200,26 @@ export default {
     formatDate(date) {
       if (!date) {
         this.$alert("请选择时间", "提示", {
-          confirmButtonText: "确定",
+          confirmButtonText: "确定"
         });
 
         return;
       }
+      date = new Date(date)
+      console.log(date)
+      var y = date.getFullYear();
+      var M = date.getMonth() + 1;
+      var d = date.getDate();
       var h = date.getHours();
       var m = date.getMinutes();
       var s = date.getSeconds();
 
+      if (M < 10) {
+        M = "0" + M;
+      }
+      if (d < 10) {
+        d = "0" + d;
+      }
       if (h < 10) {
         h = "0" + h;
       }
@@ -158,7 +229,7 @@ export default {
       if (s < 10) {
         s = "0" + s;
       }
-      return h + ":" + m + ":" + s;
+      return y+"-"+ M +"-" + d+" " + h + ":" + m + ":" + s;
     },
     handleEdit(index, row) {
       row.readonly = false;
@@ -173,7 +244,33 @@ export default {
     handleDelete() {},
     change(row) {
       row.readonly = false;
-    }
+    },
+    add() {
+      this.dialogFormVisible = true;
+    },
+    onSubmit(){
+      var data = this.sizeForm;
+      var that = this;
+      this.$axios
+        .post("http://localhost:8880/getInInfo/addInfo",data)
+        .then(resp => {
+          if(resp.data){
+            that.sizeForm = {};
+            that.dialogFormVisible = false;
+          }
+        })
+    },
+  },
+  created(){
+    var that = this
+    this.$axios
+        .get("http://localhost:8880/getInInfo/getInfos")
+        .then(resp => {
+          if(resp.data){
+            console.log(resp.data)
+            that.tableData = resp.data
+          }
+        })
   }
 };
 </script>
